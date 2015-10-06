8f465f984c00190bbacef4f86f6ee3b564d98978
XEmAX32 <XEmAX32@gmail.com>
2015-10-06 21:43:07 +0200

    Update Main.php

--- src/What-s-Up/Main.php
+++ src/What-s-Up/Main.php
@@ -36,24 +36,22 @@ if(!$this->whup->exist($name)){
 
 public function onCommand(CommandSender $sender, Command $command, $label, array $args){
 		switch($cmd->getName()){
-			case "wu":
+      case "wu":
       if (!($sender instanceof Player)) {
        $sender->sendMessage(TextFormat::RED . "This command must be run in game!");
     	 return true;
       }else{
       	$state = array_shift($args));
       	$this->whup->set($name, $state);
+      	$sender->setNameTag($name, $state);
       	if($lang == italian){
-      	$sender->sendMessage(TextFormat::BLUE . "[WHUP] Stato aggiornato con successo!");
+      		$sender->sendMessage(TextFormat::BLUE . "[WHUP] Stato aggiornato con successo!");
       	}
       	if($lang == english){
-      	$sender->sendMessage(TextFormat::BLUE . "[WHUP] Succesfully updated state!");
+      		$sender->sendMessage(TextFormat::BLUE . "[WHUP] Succesfully updated state!");
       	}
       	return true;
       }
     }
   }
 }
-
-      
-      
